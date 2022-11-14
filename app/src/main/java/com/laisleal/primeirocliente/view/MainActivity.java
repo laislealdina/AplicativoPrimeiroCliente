a3c642e689854abcc59682962bf9dfadab9d6c9c
CIANDT\laisdina <laisdina@id.uff.br>
2022-11-13 22:55:11 -0300

    Troca de dados entre activities com Bundle.

--- app/src/main/java/com/laisleal/primeirocliente/view/MainActivity.java
+++ app/src/main/java/com/laisleal/primeirocliente/view/MainActivity.java
@@ -4,17 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.laisleal.primeirocliente.R;
 
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = "APP_PRIMEIRO_CLIENTE";
+    private TextView txtView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: Iniciando tela Main");
+
+        Bundle bundle = getIntent().getExtras();
+
+        Log.d(TAG, "onCreate- Nome do cliente: " + bundle.getString("nome"));
+        Log.d(TAG, "onCreate- Nome do cliente: " + bundle.getString("telefone"));
+
+        txtView = findViewById(R.id.textView);
+        txtView.setText(" Bem-vindo " + bundle.getString("nome"));
     }
 }
--- app/src/main/java/com/laisleal/primeirocliente/view/TelaSplashActivity.java
+++ app/src/main/java/com/laisleal/primeirocliente/view/TelaSplashActivity.java
@@ -8,11 +8,13 @@ import android.os.Handler;
 import android.util.Log;
 
 import com.laisleal.primeirocliente.R;
+import com.laisleal.primeirocliente.model.Cliente;
 
 public class TelaSplashActivity extends AppCompatActivity {
 
     private String TAG = "APP_PRIMEIRO_CLIENTE";
     Integer tempoEspera = 1000 * 15;
+    private Cliente objCliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +26,23 @@ public class TelaSplashActivity extends AppCompatActivity {
     public void trocarTela() {
         Log.d(TAG, "trocarTela: Iniciando troca de tela..");
 
+        objCliente = new Cliente("Douglas",
+                                  "douglas@gmail.com",
+                                "(21)26117517",
+                                   35,
+                                    true);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Log.d(TAG, "trocarTela: esperando...");
                 Intent trocaDeTela = new Intent(TelaSplashActivity.this, MainActivity.class);
+
+                Bundle bundle = new Bundle();
+                bundle.putString("nome", objCliente.getNome());
+                bundle.putString("telefone", objCliente.getTelefone());
+                trocaDeTela.putExtras(bundle);
+
                 startActivity(trocaDeTela);
                 finish();
             }
